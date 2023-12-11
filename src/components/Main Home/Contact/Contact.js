a503595f9dd44c85bdfbe5ee7e85e28cf0433da1
shariful4345 <mdsajeeb4345@gmail.com>
2023-12-11 22:50:57 +0600

    update contact file

--- src/components/Main Home/Contact/Contact.js
+++ src/components/Main Home/Contact/Contact.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { BsFillSendFill } from "react-icons/bs";
+import toast from "react-hot-toast";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_uk32j5y",
+        "template_qti2sfw",
+        form.current,
+        "prvxqaezIRoVgZ_H7"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          toast.success("Your massage is successfully,  thank you so much.");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="flex md:flex-row flex-col  items-center  gap-x-6">
       <div className="md:space-y-2 space-y-1 lg:w-1/2 md:py-0 sm:py-2 py-1  w-full mx-auto text-center font-abc">
@@ -18,18 +43,24 @@ const Contact = () => {
           If you want!
         </h2>
       </div>
-      <div className="lg:w-1/2 md:w-full w-11/12 mx-auto border rounded-lg border-white">
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="lg:w-1/2 md:w-full w-11/12 mx-auto border rounded-lg border-white"
+      >
         <div className=" lg:px-4 md:px-3 px-2 lg:pt-6 pt-4 lg:pb-12 md:pb-10 pb-8 space-y-4">
           <div className=" font-abc">
             <input
               type="text"
+              name="user_name"
               placeholder="Type your Name"
-              className=" px-4 py-2 w-full  border-b-2 border-white bg-transparent  required    "
+              className=" px-4 py-2 w-full   border-b-2 border-0 bg-transparent  required    "
             />
           </div>
           <div className="">
             <input
               type="email"
+              name="user_email"
               placeholder="Type Your Email"
               className=" px-4 py-2 w-full  border-b-2 border-white bg-transparent   required  "
             />
@@ -37,6 +68,7 @@ const Contact = () => {
           <div className="">
             <input
               type="text"
+              name="message"
               placeholder="Your Massage"
               className=" px-4 py-8 w-full  border-b-2 border-white bg-transparent  hover:bg-none    "
             />
@@ -48,7 +80,7 @@ const Contact = () => {
             </button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
